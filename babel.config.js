271bf039f962439fda14866c0eee1eed80bc04e9
Khaled <khaled_abdel_razek@hotmail.com>
2024-12-26 11:42:09 +0200

    Fix Babel configuration for loose mode
    Add `loose` option to Babel plugins to fix build error.
    
    * **babel.config.js**
      - Add `loose` option to `@babel/plugin-proposal-class-properties`
      - Add `loose` option to `@babel/plugin-proposal-private-methods`
      - Add `loose` option to `@babel/plugin-proposal-private-property-in-object`
    
    * **.devcontainer/devcontainer.json**
      - Add tasks for `test`, `build`, and `launch` commands

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,9 @@ module.exports = function(api) {
     presets: ['babel-preset-expo'],
     plugins: [
       '@babel/plugin-transform-runtime',
-      '@babel/plugin-proposal-class-properties',
+      ['@babel/plugin-proposal-class-properties', { loose: true }], // Pf91d
+      ['@babel/plugin-proposal-private-methods', { loose: true }], // P33b0
+      ['@babel/plugin-proposal-private-property-in-object', { loose: true }], // P2fb7
       '@babel/plugin-proposal-object-rest-spread'
     ]
   };
